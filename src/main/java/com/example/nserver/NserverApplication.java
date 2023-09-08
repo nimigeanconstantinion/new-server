d46bf393995fef34d3c92df4dd9e103ecdfb5381
Ion Constantin Nimigean <constantin.nimigean@gmail.com>
2023-09-08 12:28:48 +0300

    first commit

---
+++ src/main/java/com/example/nserver/NserverApplication.java
@@ -0,0 +1,13 @@
+package com.example.nserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NserverApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NserverApplication.class, args);
+	}
+
+}
